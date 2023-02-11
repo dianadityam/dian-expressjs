8cc3ef1eaca378450cd6fac8e17690e286143006
Dian Aditya <dian.aditya53@gmail.com>
2023-02-11 13:32:33 +0700

    express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const router = require('./routes.js');
+const log = require ('./middlewares/logger');
+const port = 3000;
+
+app.use(log);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(router);
+// app.use((req, res, next) => {
+//     res.status(404); 
+//     res.send({
+//         status: 'failed',
+//         message: 'Resource ' + req.originalUrl + ' Not Found'
+//     });
+// });
+
+app.listen(port,() => {
+    console.log(`App listening on port ${port}`);
+});
